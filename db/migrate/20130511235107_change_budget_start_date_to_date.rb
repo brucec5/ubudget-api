d8a4b060d746dfbb110c31847778c0596c2ef5d5
brucec5 <aprofessionalemailaddress@gmail.com>
2013-05-11 17:19:28 -0700

    Fix the start date problem for real

---
+++ db/migrate/20130511235107_change_budget_start_date_to_date.rb
@@ -0,0 +1,9 @@
+class ChangeBudgetStartDateToDate < ActiveRecord::Migration
+  def up
+    change_column :budgets, :start_date, :date
+  end
+
+  def down
+    change_column :budgets, :start_date, :datetime
+  end
+end
