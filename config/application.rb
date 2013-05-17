f25a4afd4dadedc30161cfe53b827dae599a8326
brucec5 <aprofessionalemailaddress@gmail.com>
2013-05-16 19:54:55 -0700

    Disable the root node for all active record json serialization

--- config/application.rb
+++ config/application.rb
@@ -53,6 +53,9 @@ module UbudgetApi
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = true
 
+    # Disable to_json/as_json from emitting the root node (the model name)
+    config.active_record.include_root_in_json = false
+
     # Enable the asset pipeline
     config.assets.enabled = true
 
